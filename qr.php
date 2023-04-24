8f066d918d16828cad939c892587042981080d66
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-24 22:25:36 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -1,6 +1,23 @@
 <?php
+// Define the database connection parameters
+$servername = "encode99.com.lk";
+$username = "encodeco_lms";
+$password = "%Lms%1234@Susipwin";
+$database = "encodeco_lms";
+
+// Create a new MySQLi instance and connect to the database
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+ 
+
 $code = $_POST['code'];
 // Do something with $code
+
+
 
 echo $code;
 ?>
