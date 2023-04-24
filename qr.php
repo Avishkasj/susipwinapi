926070482b1c6f8ffdde36fa70928622df46bc58
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-24 22:48:26 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -1,15 +1,11 @@
 <?php
+// Retrieve the QR code value passed from the previous page
+$qr_code_value = $_GET['qr_code'];
 
-
-$code = $_POST['code'];
-// Do something with $code
-
-if($code == null){
-    echo "Code is null";
+// Process the QR code value (example code)
+if ($qr_code_value == null) {
+  echo 'QR code value is null';
+} else {
+  echo 'QR code value is notnull';
 }
-else{
-    echo "code is not null";
-}
-
-echo $code;
 ?>
