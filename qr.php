1916cd8d00f33f882a09fdb6de7dc9d43233c2b4
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-25 21:56:01 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -1,4 +1,18 @@
 <?php
+
+$servername = "encode99.com.lk";
+$username = "encodeco_lms";
+$password = "%Lms%1234@Susipwin";
+$database = "encodeco_lms";
+
+// Create a new MySQLi instance and connect to the database
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 // Retrieve the data sent from the mobile app
 if(isset($_POST['data'])) {
     $json_data = $_POST['data'];
@@ -22,5 +36,23 @@ if(isset($_POST['data'])) {
     $response = array('error' => 'No data received');
     echo json_encode($response);
 }
+
+$sql = "SELECT sfullname FROM students";
+$result = $conn->query($sql);
+
+// Convert the data to a JSON array
+$data = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+// Send the JSON response back to the Flutter app
+header('Content-Type: application/json');
+echo json_encode($data);
+
+// Close the database connection
+$conn->close();
 
 ?>
