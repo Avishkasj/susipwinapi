0627652c6748226f848bdc2d2b8658826eccfa30
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-25 21:36:24 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -3,19 +3,16 @@
 if (isset($_POST['data'])) {
   // Data has been received
   $qr_data = $_POST['data'];
-
+  
   // Process the data here
   // ...
-
-  // Return a JSON response with the QR code data
-  $response = array('qr_data' => $qr_data);
-  header('Content-Type: application/json');
-  echo json_encode($response);
+  
+  // Return a response if necessary
+  echo "Data received: " . $qr_data;
 } else {
   // No data received
   echo "No data received";
 }
-
 
 ?>
 
