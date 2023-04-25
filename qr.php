941987c5785bbe5a93ba3837aa2435ae310924b0
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-25 21:52:04 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -1,18 +1,26 @@
 <?php
+// Retrieve the data sent from the mobile app
+if(isset($_POST['data'])) {
+    $json_data = $_POST['data'];
 
-if (isset($_POST['data'])) {
-  // Data has been received
-  $qr_data = $_POST['data'];
-  
-  // Process the data here
-  // ...
-  
-  // Return a response if necessary
-  echo "Data received: " . $qr_data;
+    // Decode the JSON data into a PHP associative array
+    $data = json_decode($json_data, true);
+    
+    // Assign each value to a separate variable
+    $user_id = $data['userid'];
+    // $course_id = $data['courses'];
+    // $id = $data['id'];
+    
+    // Process the data here
+    // ...
+    
+    // Send a response back to the mobile app
+    $response = array('message' => 'Data received: ' . $json_data);
+    echo json_encode($response);
 } else {
-  // No data received
-  echo "No data received";
+    // No data received
+    $response = array('error' => 'No data received');
+    echo json_encode($response);
 }
 
 ?>
-
