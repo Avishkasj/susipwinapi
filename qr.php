a66d072104af66203c5707170c5bba7d87e9961b
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-26 09:05:25 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -15,37 +15,42 @@ if ($conn->connect_error) {
 
 // Retrieve the data sent from the mobile app
 if(isset($_POST['data'])) {
-  $json_data = $_POST['data'];
-  $data = json_decode($json_data, true);
-  $user_id = $data['userid'];
-  
-  // Process the data here
-  // ...
-  
-  // Send a response back to the mobile app
-  $response = array('message' => 'User ID received: ' . $user_id);
-  echo json_encode($response);
+    $json_data = $_POST['data'];
+
+    // Decode the JSON data into a PHP associative array
+    $data = json_decode($json_data, true);
+    
+    // Assign each value to a separate variable
+    $user_id = $data['userid'];
+    // $course_id = $data['courses'];
+    // $id = $data['id'];
+    
+    // Process the data here
+    // ...
+    
+    // Send a response back to the mobile app
+    $response = array('message' => 'Data received: ' . $json_data);
+    echo json_encode($response);
 } else {
-  // No data received
-  $response = array('error' => 'No data received');
-  echo json_encode($response);
+    // No data received
+    $response = array('error' => 'No data received');
+    echo json_encode($response);
 }
 
-
-$sql = "SELECT sfullname FROM students WHERE id='8';";
+$sql = "SELECT sfullname FROM students WHERE id='$user_id";
 $result = $conn->query($sql);
 
 // Convert the data to a JSON array
-$data2 = array();
+$data = array();
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        $data2[] = $row;
+        $data[] = $row;
     }
 }
 
 // Send the JSON response back to the Flutter app
 header('Content-Type: application/json');
-echo json_encode($data2);
+echo json_encode($data);
 
 // Close the database connection
 $conn->close();
