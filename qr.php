065ccd3b5a368de51b24eac43e9fc89e8c52ddcd
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-26 08:30:44 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -15,27 +15,22 @@ if ($conn->connect_error) {
 
 // Retrieve the data sent from the mobile app
 if(isset($_POST['data'])) {
-    $json_data = $_POST['data'];
-
-    // Decode the JSON data into a PHP associative array
-    $data = json_decode($json_data, true);
-    
-    // Assign each value to a separate variable
-    $user_id = $data['userid'];
-    // $course_id = $data['courses'];
-    // $id = $data['id'];
-    
-    // Process the data here
-    // ...
-    
-    // Send a response back to the mobile app
-    $response = array('message' => 'Data received: ' . $json_data);
-    echo json_encode($response);
+  $json_data = $_POST['data'];
+  $data = json_decode($json_data, true);
+  $user_id = $data['userid'];
+  
+  // Process the data here
+  // ...
+  
+  // Send a response back to the mobile app
+  $response = array('message' => 'User ID received: ' . $user_id);
+  echo json_encode($response);
 } else {
-    // No data received
-    $response = array('error' => 'No data received');
-    echo json_encode($response);
+  // No data received
+  $response = array('error' => 'No data received');
+  echo json_encode($response);
 }
+
 
 $sql = "SELECT sfullname FROM students WHERE id='$user_id'";
 $result = $conn->query($sql);
