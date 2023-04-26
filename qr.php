3d7f71d523d46307901ae9594dab4816563f8913
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-26 08:40:00 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -32,7 +32,7 @@ if(isset($_POST['data'])) {
 }
 
 
-$sql = "SELECT sfullname FROM students WHERE id=8";
+$sql = "SELECT sfullname FROM students ";
 $result = $conn->query($sql);
 
 // Convert the data to a JSON array
