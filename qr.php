a5d1a115d7c581d24bec1dba1a49677d764d369c
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-26 09:01:21 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -36,16 +36,16 @@ $sql = "SELECT sfullname FROM students WHERE id='8';";
 $result = $conn->query($sql);
 
 // Convert the data to a JSON array
-$data = array();
+$data2 = array();
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        $data[] = $row;
+        $data2[] = $row;
     }
 }
 
 // Send the JSON response back to the Flutter app
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($data2);
 
 // Close the database connection
 $conn->close();
