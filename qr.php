5cb3dd2dc94e32ba2594f523ccf727de722afa9d
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-04-26 16:39:38 +0530

    Update qr.php

--- qr.php
+++ qr.php
@@ -28,7 +28,7 @@ if(isset($_POST['data'])) {
     // Process the data here
     // ...
     
-    $sql = "SELECT sfullname FROM students WHERE userId='$user_id'";
+    $sql = "SELECT * FROM students WHERE userId='$user_id'";
     $result = $conn->query($sql);
 
     // Convert the data to a JSON array
