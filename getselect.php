481396c9de473c3296299230e1ea3899be45f775
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-01 09:30:32 +0530

    Create getselect.php

---
+++ getselect.php
@@ -0,0 +1,19 @@
+<?php
+
+// Retrieve the selected_option parameter from the POST request
+$selectedOption = $_POST['selected_option'] ?? '';
+
+// Do something with the selected_option value (e.g. save it to a database)
+// ...
+
+// Prepare a JSON response
+$response = ['status' => 'success', 'message' => 'Received selected option: ' . $selectedOption];
+$jsonResponse = json_encode($response);
+
+// Set the content type header to application/json
+header('Content-Type: application/json');
+
+// Send the JSON response back to the Flutter app
+echo $jsonResponse;
+
+?>
