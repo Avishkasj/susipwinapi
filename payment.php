92701ae23562346e6ff755c87dd09799bda5fc16
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-01 14:15:54 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -5,7 +5,7 @@ $username = "encodeco_lms";
 $password = "%Lms%1234@Susipwin";
 $database = "encodeco_lms";
 
-$uid = "23";
+$uid = "8";
 
 // Create a new MySQLi instance and connect to the database
 $conn = new mysqli($servername, $username, $password, $database);
