7fbd7fa761829db71392631ffcebd4b1103b20f1
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-05-30 21:45:04 +0530

    Update payment.php

--- payment.php
+++ payment.php
@@ -9,6 +9,7 @@ $database = "encodeco_lms";
 $user = new User();
 $uid = $user->getUserId();
 
+
 // Create a new MySQLi instance and connect to the database
 $conn = new mysqli($servername, $username, $password, $database);
 
@@ -18,43 +19,52 @@ if ($conn->connect_error) {
 }
 
 // Retrieve the data sent from the mobile app
-if (isset($_POST['data']) && isset($_POST['name'])) {
+if(isset($_POST['data'])) {
     $selectedOption = $_POST['data'];
-    $name = $_POST['name'];
 
     // Process the data here
     // ...
+    //stable version
 
-    // $sql = "SELECT p.* 
-    // FROM payments p 
-    // INNER JOIN students s ON p.suid = s.id 
-    // INNER JOIN courses c ON p.cid = c.id 
-    // WHERE s.userId = '$uid' 
-    // AND c.coursename = '$selectedOption'
-    // AND p.month = '5';
-    // ";
+    // $sql2 = "SELECT id FROM students WHERE userId = '$user_id' ";
+    // $sql3 = "SELECT id FROM courses WHERE coursename = '$selectedOption'";
+    // $sql4 = "SELECT * FROM payments WHERE cid = '$sql3' AND suid='$sql2' AND month='5'";
 
-     $sql = "SELECT * FROM users WHERE username= $name
+
+    $sql = "SELECT p.* 
+    FROM payments p 
+    INNER JOIN students s ON p.suid = s.id 
+    INNER JOIN courses c ON p.cid = c.id 
+    WHERE s.userId = '$uid' 
+    AND c.coursename = '$selectedOption'
+    AND p.month = '5';
     ";
 
 
+
+
     // Fetch the data from the database
+    // $sql = "SELECT * FROM courses WHERE coursename = '$selectedOption'";
+
     $result = $conn->query($sql);
 
     // Check if any rows were returned
     if ($result->num_rows > 0) {
-        // Fetch the first row of data
-        $row = $result->fetch_assoc();
-
+        // Create an array to store the data
+        $data = array();
+    
+        // Loop through each row
+        while ($row = $result->fetch_assoc()) {
+            // Add the row data to the array
+            $data[] = array(
+                'id' => $row['id'],
+                'coursename' => $row['coursename'],
+                'description' => $row['description'],
+                // Add any other fields you want to include here
+            );
+        }
+    
         // Send the JSON response back to the Flutter app
-        $data = array(
-            // 'id' => $row['id'],
-            // 'coursename' => $row['coursename'],
-            // 'description' => $row['description'],
-            'email' => $row['email'],
-            // Add any other fields you want to include here
-        );
-
         header('Content-Type: application/json');
         echo json_encode($data);
     } else {
