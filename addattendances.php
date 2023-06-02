ad983106f7f69e8b192509f50a66796d4b38532f
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-06-02 22:00:50 +0530

    Update addattendances.php

--- addattendances.php
+++ addattendances.php
@@ -31,8 +31,8 @@ if ($result) {
     $cid = $course['id'];
 
 
-    $insertSql = "INSERT INTO attendances(auserid, acourseid) 
-    VALUES (1, 2)";
+    $insertSql = "INSERT INTO attendances (id, auserid, acourseid, aday, atime, createdAt, updatedAt) 
+    VALUES (1, 1, 1, 2022/2/2, 2022/2/2, 2022/2/2, 2022/2/2)";
     $insertResult = mysqli_query($conn, $insertSql);
 
     if ($insertResult) {
