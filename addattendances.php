319aed3ccb5169f9df335fabf842926acf8c3323
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-06-03 00:20:23 +0530

    Update addattendances.php

--- addattendances.php
+++ addattendances.php
@@ -17,41 +17,44 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if(isset($_POST['data'])&&isset($_POST['name'])) {
+if (isset($_POST['data']) && isset($_POST['name'])) {
     $selectedOption = $_POST['data'];
     $uname = $_POST['name'];
 
-$sql2 = "SELECT id FROM courses WHERE coursename = ?";
-$stmt = $conn->prepare($sql2);
-$stmt->bind_param("s", $selectedOption);
-$stmt->execute();
-$result = $stmt->get_result();
+    $sql2 = "SELECT id FROM courses WHERE coursename = ?";
+    $stmt = $conn->prepare($sql2);
+    $stmt->bind_param("s", $selectedOption);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $courseId = $row['id'];
 
-   
+        $sql = "INSERT INTO attendances (auserid, acourseid, aday, atime, createdAt, updatedAt)
+                VALUES (?, ?, '2023-06-02', '09:00:00', NOW(), NOW())";
 
-$sql = "INSERT INTO attendances (auserid, acourseid, aday, atime, createdAt, updatedAt)
-VALUES ($uname,$result, '2023-06-02', '09:00:00', NOW(), NOW());
-";
-$result = mysqli_query($conn, $sql);
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ii", $uname, $courseId);
+        $stmt->execute();
 
-
-    if ($result) {
-        echo "Data inserted successfully into the attendances table.";
-        $check = "Mark";
-        header('Content-Type: application/json');
-        echo json_encode($check);
-      }
-      else{
+        if ($stmt->affected_rows > 0) {
+            $check = "Mark";
+            header('Content-Type: application/json');
+            echo json_encode($check);
+        } else {
+            $check = "Not Mark";
+            echo json_encode($check);
+        }
+    } else {
         $check = "Not Mark";
-        $response = array('error' => 'No data found');
         echo json_encode($check);
-      }
-
-
-
+    }
 }
-// Close the database connection
+
+// Close the prepared statement and database connection
+$stmt->close();
 $conn->close();
 ?>
+
 
