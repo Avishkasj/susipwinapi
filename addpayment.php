126b25c4fe73fb2aa27a112560b6ed9c6f4de371
Firstname Lastname <asupunjayasooriya@gmail.com>
2023-07-25 01:29:45 +0530

    Update addpayment.php

--- addpayment.php
+++ addpayment.php
@@ -52,6 +52,77 @@ if (isset($_POST['data']) && isset($_POST['name'])) {
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("ii", $courseId, $uname);
             $stmt->execute();
+
+
+            //send sms
+            $sql4 = "SELECT parentId FROM parentstudents WHERE studentid = ?";
+            $stmt = $conn->prepare($sql4);
+            $stmt->bind_param("s", $uname);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $parent = $row['parentId'];
+
+                $sql5 = "SELECT tel FROM users WHERE id = ?";
+                $stmt2 = $conn->prepare($sql5);
+                $stmt2->bind_param("ii", $parent);
+                $stmt2->execute();
+                $result = $stmt2->get_result();
+
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $tel = $row['tel'];
+                    
+
+
+
+                    $api_url = 'http://sender.zirconhost.com/api/v2/send.php';
+                    $user_id = 105082;
+                    $api_key = 'xv8np326kfaw3uqjt';
+                    $sender_id = 'Encode99';
+                    $to = '0762697156';
+                    $message = 'hello';
+
+                    // Create the query string with the parameters
+                    $query_string = http_build_query([
+                        'user_id' => $user_id,
+                        'api_key' => $api_key,
+                        'sender_id' => $sender_id,
+                        'to' => $to,
+                        'message' => $message,
+                    ]);
+
+                    // Construct the full URL with the query string
+                    $request_url = $api_url . '?' . $query_string;
+
+                    // Send the GET request to the API
+                    $response = file_get_contents($request_url);
+
+                    // Check if the request was successful
+                    if ($response !== false) {
+                        // API returned a response, you can check and process it here
+                        echo "SMS sent successfully!";
+                    } else {
+                        // Request failed
+                        echo "Failed to send SMS.";
+                    }
+
+
+
+
+
+                }
+
+
+
+            }
+
+
+
+
+
         // The result set is empty
         if ($stmt->affected_rows > 0) {
             $check = "Mark";
